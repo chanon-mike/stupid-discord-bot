c1b98878c4874ff07e49d352c3fe4981ff65961f
chanon-mike <zzmikezz8520@gmail.com>
2023-10-28 17:11:56 +0900

    fix: remove lambda

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/chanon-mike/stupid-discord-bot/discord"
 )
 
 func main() {
-	lambda.Start(discord.Start)
+	discord.Start()
 }
